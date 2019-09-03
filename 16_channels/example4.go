f2c86667a53571929c9bd68f4d7a7af3bf913c5a
DustinKLo <dustinlo.pcc@gmail.com>
2019-09-03 10:47:48 -0700

    redid 16_channels example 3 with waitgroups

---
+++ 16_channels/example4.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+const (
+	logInfo    = "INFO"
+	logWarning = "WARNING"
+	logError   = "ERROR"
+)
+
+type logEntry struct {
+	time     time.Time
+	severity string
+	message  string
+}
+
+var wg = sync.WaitGroup{}
+var logCh = make(chan logEntry, 50)
+
+func main() {
+	wg.Add(2)
+	go addToLogger()
+	go logger()
+	wg.Wait()
+}
+
+func addToLogger() {
+	logCh <- logEntry{time.Now(), logInfo, "app is starting"}
+	logCh <- logEntry{time.Now(), logInfo, "app is shutting down"}
+	close(logCh)
+	wg.Done()
+}
+
+func logger() {
+	for entry := range logCh {
+		fmt.Printf("%v - [%v] %v\n", entry.time.Format("2006-01-02T15:04:05"), entry.severity, entry.message)
+	}
+	wg.Done()
+}
